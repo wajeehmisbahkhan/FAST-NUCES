568fb98f5c2bbe36e8e5e018309c1a7b342ad69b
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-09-19 23:30:19 +0500

    Working on side menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'admin', loadChildren: './admin/admin.module#AdminPageModule' },
 ];
 
 @NgModule({
