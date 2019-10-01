bd48e2ede817be3339e02aa547aadda76b813dd0
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-10-01 19:01:18 +0500

    Added edit component

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-edit',
+  selector: 'edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss'],
+  styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  // Coming from table component
+  @Input() element: any;
+  // For local form usage
+  localElement: any;
+  constructor(
+    // private db: DatabaseService,
+    private poc: PopoverController // private as: AlertService
+  ) {}
 
-  constructor() { }
+  ngOnInit() {
+    // Creating a deep copy for local use
+    this.localElement = JSON.parse(JSON.stringify(this.element));
+  }
 
-  ngOnInit() {}
+  updateElement() {
+    this.element.accountHolder = this.localElement.accountHolder;
+    this.element.particulars = this.localElement.particulars;
+    // this.db.updateAccount(this.cashAccount);
+    this.poc.dismiss();
+  }
 
+  deleteElement() {
+    // this.as.confirmation(
+    //   'Are you sure you want to delete this account?',
+    //   // Confirmation handler
+    //   () => {
+    //     this.db.deleteAccount(this.cashAccount);
+    //     this.poc.dismiss();
+    //   }
+    // );
+  }
+
+  editFormChanged() {
+    return (
+      this.localElement.particulars !== this.element.particulars ||
+      this.localElement.accountHolder !== this.element.accountHolder
+    );
+  }
 }
