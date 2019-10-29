056dbb5d6f1164968489b76f9d72dfd4ae818a78
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-10-29 23:14:39 +0500

    Sections got a complete makeover

--- src/app/admin/sections/sections.page.ts
+++ src/app/admin/sections/sections.page.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from 'src/app/services/server.service';
-import { Section } from 'src/app/services/helper-classes';
+import { Section, AggregateSection } from 'src/app/services/helper-classes';
 
 @Component({
   selector: 'app-sections',
   templateUrl: './sections.page.html',
   styleUrls: ['./sections.page.scss']
 })
+/*
+  Important!
+  normalSection for viewing
+  atomicSection for storing
+  aggregateSection for in between
+*/
 export class SectionsPage implements OnInit {
+  // Normal section as input
   section: Section;
   constructor(private server: ServerService) {
     this.section = new Section();
@@ -16,10 +23,25 @@ export class SectionsPage implements OnInit {
   ngOnInit() {}
 
   addSection() {
-    this.server.addPrimitiveObject('sections', this.section);
+    // Convert to atomic before sending
+    const atomicSections = AggregateSection.normalToAtomicSections(
+      this.section
+    );
+    // Push both atoms
+    atomicSections.forEach(atomicSection => {
+      this.server.addPrimitiveObject('sections', atomicSection);
+    });
   }
 
+  // Convert to aggregate to store both infos
+  get aggregateSections() {
+    // Send copy
+    const sections = JSON.parse(JSON.stringify(this.server.sections));
+    return AggregateSection.atomicToAggregateSections(sections);
+  }
+
+  // Convert to normal to show
   get sections() {
-    return this.server.sections;
+    return AggregateSection.aggregateToNormalSections(this.aggregateSections);
   }
 }
