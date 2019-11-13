3fe27d5c8ec447daf780afc082db7ce46efe7ffd
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-11-13 17:35:11 +0500

    Edit for entries ✅

--- src/app/admin/timetable/entries/edit/edit.component.ts
+++ src/app/admin/timetable/entries/edit/edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { TCSEntry } from 'src/app/services/helper-classes';
+import {
+  TCSEntry,
+  AggregateSection,
+  sortAlphaNum
+} from 'src/app/services/helper-classes';
 import { ServerService } from 'src/app/services/server.service';
 import { PopoverController } from '@ionic/angular';
 import { AlertService } from 'src/app/services/alert.service';
@@ -44,6 +48,42 @@ export class EditComponent implements OnInit {
         this.server.deletePrimitiveObject('entries', this.element.id);
         this.poc.dismiss();
       }
+    );
+  }
+
+  // Auto fill
+  fillSectionName() {
+    let sectionName = '';
+    if (!this.localElement.hasAtomicSections) {
+      this.normalSections.forEach(section => {
+        // Show all sections which are included
+        if (this.localElement.sectionIds.includes(section.id)) {
+          // Comma for multiple sections
+          if (sectionName.length > 0) {
+            sectionName += ', ';
+          }
+          sectionName += section.name;
+        }
+      });
+    }
+    this.localElement.name = sectionName;
+  }
+
+  getCourseById(id: string) {
+    return this.courses.find(course => course.id === id);
+  }
+
+  get normalSections() {
+    return AggregateSection.aggregateToNormalSections(
+      // Send copy not original to avoid changeafterread error
+      AggregateSection.atomicToAggregateSections(this.sections.slice(0))
+    ).sort((a, b) => sortAlphaNum(a.name, b.name)); // Sorted
+  }
+
+  get repeatCourse() {
+    return (
+      this.getCourseById(this.localElement.courseId) &&
+      this.getCourseById(this.localElement.courseId).isRepeatCourse
     );
   }
 
