0fb20ecb01d61e59fe05e8798282203a83419ea8
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-11-13 14:25:45 +0500

    Added all necessary input for making entry

--- src/app/admin/timetable/entries/entries.page.ts
+++ src/app/admin/timetable/entries/entries.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from 'src/app/services/server.service';
-import { TCSEntry } from 'src/app/services/helper-classes';
+import {
+  TCSEntry,
+  AggregateSection,
+  sortAlphaNum
+} from 'src/app/services/helper-classes';
 import { PopoverController } from '@ionic/angular';
 import { EditComponent } from './edit/edit.component';
 
@@ -11,15 +15,24 @@ import { EditComponent } from './edit/edit.component';
 })
 export class EntriesPage implements OnInit {
   entry: TCSEntry;
+  atomic: boolean; // For section selection
+  repeat: boolean; // For successor course selection
 
   constructor(private server: ServerService, private poc: PopoverController) {
     this.entry = new TCSEntry();
+    // Should show aggregate by default
+    this.atomic = false;
+    this.repeat = false;
   }
 
   ngOnInit() {}
 
   addEntry() {
-    this.server.addPrimitiveObject('entries', this.entry);
+    const entry: TCSEntry = JSON.parse(JSON.stringify(this.entry));
+    entry.sectionIds = this.ensureNormalSectionIds(entry.sectionIds);
+    this.server.addPrimitiveObject('entries', entry);
+    // Reset entry
+    this.entry = new TCSEntry();
   }
 
   async presentPopover(element: any) {
@@ -48,6 +61,24 @@ export class EntriesPage implements OnInit {
     return this.sections.find(section => section.id === id);
   }
 
+  ensureNormalSectionIds(sectionIds: Array<string>) {
+    const normalSectionIds: Array<string> = [];
+    sectionIds.forEach(sectionId => {
+      if (sectionId.includes('\n')) {
+        // Convert to single array containing atomic values
+        sectionId.split('\n').forEach(id => normalSectionIds.push(id));
+      } else {
+        normalSectionIds.push(sectionId);
+      }
+    });
+    return normalSectionIds;
+  }
+
+  // Auto fill
+  fillSectionName() {
+    console.log(this.entry.sectionIds);
+  }
+
   get teachers() {
     return this.server.teachers;
   }
@@ -58,6 +89,13 @@ export class EntriesPage implements OnInit {
 
   get sections() {
     return this.server.sections;
+  }
+
+  get normalSections() {
+    return AggregateSection.aggregateToNormalSections(
+      // Send copy not original to avoid changeafterread error
+      AggregateSection.atomicToAggregateSections(this.sections.slice(0))
+    ).sort((a, b) => sortAlphaNum(a.name, b.name)); // Sorted
   }
 
   get entries() {
