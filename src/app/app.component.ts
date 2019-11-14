cffa4b26568c2302a19e63c2d39bcdf97b085a12
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-11-14 19:44:47 +0500

    Enabled firestore persistence 🔥

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ServerService } from './services/server.service';
+import { firestore } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -24,6 +25,21 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      // Data persistence
+      firestore()
+        .enablePersistence()
+        .catch(err => {
+          if (err.code === 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+          } else if (err.code === 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+          }
+          console.log(err);
+        });
       // TODO: If logging in to admin
       this.server.loadPrimitiveObjects();
     });
