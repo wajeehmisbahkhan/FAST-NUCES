8493bd4ade9d7dd18c6237d5f9b74f1505aea61a
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-11-25 19:44:54 +0500

    Deleted student route 🙇‍♂️

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,6 @@ const routes: Routes = [
     path: 'admin',
     loadChildren: () =>
       import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
-  },
-  {
-    path: 'student',
-    loadChildren: () =>
-      import('./student/student-routing.module').then(
-        m => m.StudentRoutingModule
-      )
   }
 ];
 
