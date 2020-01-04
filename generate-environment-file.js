1d66e94ad3de74dfc4c3c0bbe13c313f74267021
Wajeeh Misbah Khan <37481915+wajeehmisbahkhan@users.noreply.github.com>
2020-01-04 21:08:27 +0500

    Update generate-environment-file.js

--- generate-environment-file.js
+++ generate-environment-file.js
@@ -1,8 +1,8 @@
-import { writeFile } from 'fs';
+const fs = require('fs');
 const targetPath = './src/environments/environment.prod.ts';
 // `environment.ts` file structure
 const envConfigFile = process.env.secrets.ENVIRONMENT_PROD_CONTENT;
-writeFile(targetPath, envConfigFile, function (err) {
+fs.writeFile(targetPath, envConfigFile, function (err) {
    if (err) {
        throw console.error(err);
    } else {
