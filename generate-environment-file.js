42423cf7c8ec42bfd8ad62aeccbc424b797c38b4
Wajeeh Misbah Khan <37481915+wajeehmisbahkhan@users.noreply.github.com>
2020-01-04 21:25:28 +0500

    Generating prod and prod.ts

--- generate-environment-file.js
+++ generate-environment-file.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
-const targetPath = './src/environments/environment.prod.ts';
-// `environment.ts` file structure
-const envConfigFile = process.env.ENVIRONMENT_PROD_CONTENT;
-fs.writeFile(targetPath, envConfigFile, function (err) {
-   if (err) {
-       throw console.error(err);
-   } else {
-       console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
-   }
+// Environment paths
+const targetPath = './src/environments/environment.ts';
+const productionTargetPath = './src/environments/environment.prod.ts';
+// File structures
+const envConfigFile = process.env.ENVIRONMENT_CONTENT;
+const envProdConfigFile = process.env.ENVIRONMENT_PROD_CONTENT;
+// Write
+fs.writeFile(targetPath, envConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.ts file generated correctly at ${targetPath} \n`
+    );
+  }
+});
+fs.writeFile(targetPath, envProdConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.prod.ts file generated correctly at ${targetPath} \n`
+    );
+  }
 });
