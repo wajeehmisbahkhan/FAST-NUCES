c84f946b74712514d7132b5ece5aa1fc4d125216
Wajeeh Misbah Khan <37481915+wajeehmisbahkhan@users.noreply.github.com>
2020-01-04 22:45:26 +0500

    Added promise waiting

--- generate-environment-file.js
+++ generate-environment-file.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 // Environment paths
 const targetPath = './src/environments/environment.ts';
 const productionTargetPath = './src/environments/environment.prod.ts';
@@ -6,7 +6,7 @@ const productionTargetPath = './src/environments/environment.prod.ts';
 const envConfigFile = process.env.ENVIRONMENT_CONTENT;
 const envProdConfigFile = process.env.ENVIRONMENT_PROD_CONTENT;
 // Write
-fs.writeFile(targetPath, envConfigFile, function(err) {
+await fs.writeFile(targetPath, envConfigFile, function(err) {
   if (err) {
     throw console.error(err);
   } else {
@@ -15,12 +15,12 @@ fs.writeFile(targetPath, envConfigFile, function(err) {
     );
   }
 });
-fs.writeFile(productionTargetPath, envProdConfigFile, function(err) {
+await fs.writeFile(productionTargetPath, envProdConfigFile, function(err) {
   if (err) {
     throw console.error(err);
   } else {
     console.log(
-      `Angular environment.prod.ts file generated correctly at ${targetPath} \n`
+      `Angular environment.prod.ts file generated correctly at ${productionTargetPath} \n`
     );
   }
 });
