ab92b981465a6d5b4315599a284a2e36a177e727
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-01-04 16:33:56 +0500

    Added test timetable

--- src/app/admin/timetable/generate/generate.page.ts
+++ src/app/admin/timetable/generate/generate.page.ts
@@ -11,25 +11,62 @@ export class GeneratePage implements OnInit {
   departments: Array<string>;
   timetables: Array<Array<Lecture>>;
 
-  constructor(private server: ServerService) {}
+  generating: boolean;
+
+  constructor(private server: ServerService) {
+    // TODO: Set by server
+    this.generating = false;
+  }
 
   ngOnInit() {
-    this.departments = [];
-    this.timetables = [];
+    this.departments = ['CS'];
+    const csLecture = new Lecture();
+    csLecture.assignedSlots = [
+      {
+        day: 0,
+        roomId: this.server.rooms[0].id,
+        time: 0
+      }
+    ];
+    csLecture.atomicSectionIds = [this.server.atomicSections[0].id];
+    csLecture.courseId = this.courses[0].id;
+    csLecture.id = 'cs-lecture';
+    csLecture.name = 'GR1';
+    csLecture.strength = 50;
+    csLecture.teacherIds = [this.server.teachers[0].id];
+    this.timetables = [
+      // CS
+      [csLecture]
+    ];
+  }
+
+  generateTimetable() {
+    this.generating = true;
+    // TODO: Send request to server
+    setTimeout(this.server.generateTimeTable.bind(this.server), 1000);
+    setTimeout(this.generateTimetablesAndDepartments.bind(this), 1200);
+  }
+
+  generateTimetablesAndDepartments() {
     // Departments in timetable
-    this.timetable.forEach(lecture => {
+    this.generated.forEach(lecture => {
       const department = this.getCourseById(lecture.courseId).department;
       if (!this.departments.includes(department))
         this.departments.push(department);
     });
     // Timetables based on departments
     this.departments.forEach(department => {
-      const timetable = this.timetable.filter(
+      const timetable = this.generated.filter(
         lecture =>
           this.getCourseById(lecture.courseId).department === department
       );
       this.timetables.push(timetable);
     });
+    this.generating = false;
+  }
+
+  cancel() {
+    // TODO: Send request to server
   }
 
   getCourseById(id: string) {
@@ -40,7 +77,7 @@ export class GeneratePage implements OnInit {
     return this.server.courses;
   }
 
-  get timetable() {
-    return this.server.timetable;
+  get generated() {
+    return this.server.generated;
   }
 }
