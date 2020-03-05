d367d964b9b315f248a4143eed4ded5371c63752
Wajeeh Misbah Khan <37481915+wajeehmisbahkhan@users.noreply.github.com>
2020-03-05 11:13:14 +0500

    Create build.yml (#1)
    * Create build.yml
    
    * Update build.yml
    
    * Added dotenv package
    
    * Create generate-environment-file.js
    
    * Update build.yml
    
    * Update generate-environment-file.js
    
    * Made production build
    
    * Replaced process.secrets.VAR with process.VAR
    
    * Generating prod and prod.ts
    
    * Fixed filepath
    
    * Added promise waiting
    
    * Only build Node 12
    
    * Removed promises
    
    * Update generate-environment-file.js
    
    * Update generate-environment-file.js
    
    * Update generate-environment-file.js
    
    * Update generate-environment-file.js
    
    * Update build.yml

---
+++ generate-environment-file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+// Environment paths
+const targetPath = './src/environments/environment.ts';
+const productionTargetPath = './src/environments/environment.prod.ts';
+// File structures
+const envConfigFile = process.env.ENVIRONMENT_CONTENT;
+const envProdConfigFile = process.env.ENVIRONMENT_PROD_CONTENT;
+// Write
+fs.writeFile(targetPath, envConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.ts file generated correctly at ${targetPath} \n`
+    );
+  }
+});
+fs.writeFile(productionTargetPath, envProdConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.prod.ts file generated correctly at ${productionTargetPath} \n`
+    );
+  }
+});
